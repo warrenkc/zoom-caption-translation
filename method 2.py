c307adca8039db88546254379ded0c936c924551
xellosiris <xellosiris@gmail.com>
2021-01-05 11:14:02 +0800

    update

---
+++ method 2.py
@@ -0,0 +1,195 @@
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Cloud Media Translation API sample application using a microphone.
+
+Example usage:
+    python translate_from_mic.py
+"""
+
+# [START media_translation_translate_from_mic]
+from __future__ import division
+import requests
+import time
+import itertools
+import json
+from google.cloud import mediatranslation as media
+import pyaudio
+from six.moves import queue
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+SpeechEventType = media.StreamingTranslateSpeechResponse.SpeechEventType
+
+
+class MicrophoneStream:
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk,configs,token):
+        self._rate = rate
+        self._chunk = chunk
+        self.token=token
+        
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+        self.seq_count=configs["seq_count"]
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            channels=1, rate=self._rate,
+            input=True, frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type=None, value=None, traceback=None):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def exit(self):
+        self.__exit__()
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b''.join(data)
+    def zoom_post(self,text):
+        post_params={
+                'seq' : self.seq_count,
+                'lang':"en-US"}
+        headers={'Content-type': 'text/plain; charset=utf-8'}
+        result=requests.post(self.token,params=post_params, data=text.encode('utf-8'),headers=headers)
+        if result.status_code!=200:
+            print("傳送失敗！請檢查網路連線和憑證是否正確")
+        self.seq_count=self.seq_count+1
+
+def listen_print_loop(responses,stream):
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+    """
+    translation = ''
+    # source = ''
+    for response in responses:
+        # Once the transcription settles, the response contains the
+        # END_OF_SINGLE_UTTERANCE event.
+        if (response.speech_event_type ==
+                SpeechEventType.END_OF_SINGLE_UTTERANCE) :
+
+            stream.zoom_post(translation)
+            # print(u'\nzoom translation: {0}'.format(translation))
+            # print(u'recognition result: {0}'.format(source))
+            return 0
+
+        result = response.result
+        translation = result.text_translation_result.translation
+        # source = result.recognition_result
+
+        print(u'\ntranslation: {0}'.format(translation))
+        
+        # print(u'recognition result: {0}'.format(source))
+
+
+def do_translation_loop(configs,token):
+
+    print('Begin speaking...')
+
+    client = media.SpeechTranslationServiceClient()
+
+    speech_config = media.TranslateSpeechConfig(
+        audio_encoding='linear16',
+        source_language_code=configs["source_lang"],
+        target_language_code=configs["target_lang"])
+
+    config = media.StreamingTranslateSpeechConfig(
+        audio_config=speech_config, single_utterance=True)
+
+    # The first request contains the configuration.
+    # Note that audio_content is explicitly set to None.
+    first_request = media.StreamingTranslateSpeechRequest(
+        streaming_config=config, audio_content=None)
+
+    with MicrophoneStream(RATE, CHUNK,configs,token) as stream:
+        
+        audio_generator = stream.generator()
+        mic_requests = (media.StreamingTranslateSpeechRequest(
+            audio_content=content,
+            streaming_config=config)
+            for content in audio_generator)
+
+        requests = itertools.chain(iter([first_request]), mic_requests)
+
+        responses = client.streaming_translate_speech(requests)
+
+        # Print the translation responses as they arrive
+        result = listen_print_loop(responses,stream)
+        if result == 0:
+            stream.exit()
+
+
+def main():
+    with open ("config.json") as config_file:
+        configs=json.load(config_file)
+    token=input("請輸入憑證：") 
+    while True:
+    #     print()
+    #     option = input('Press any key to translate or \'q\' to quit: ')
+
+    #     if option.lower() == 'q':
+    #         break
+
+        do_translation_loop(configs,token)
+
+
+if __name__ == '__main__':
+    main()
+# [END media_translation_translate_from_mic]
---
+++ method1.py
@@ -0,0 +1,212 @@
+#!/usr/bin/env python
+
+# [START speech_transcribe_streaming_mic]
+from __future__ import division
+import requests
+import re
+import sys
+import six
+import time
+import json
+from google.cloud import translate_v2 as translate
+from google.cloud import speech_v1p1beta1 as speech
+
+
+import pyaudio
+from six.moves import queue
+
+# Audio recording parameters
+RATE = 16000
+CHUNK = int(RATE / 10)  # 100ms
+translate_client = translate.Client()
+
+
+
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk,_configs):
+        self._rate = rate
+        self._chunk = chunk
+        self.source = _configs["source_lang"]
+        self.target = _configs["target_lang"]
+        self.seq_count=_configs["seq_count"]
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+    
+    def zoomtranslate(self,text):
+        if isinstance(text, six.binary_type):
+            text = text.decode("utf-8")
+        result =translate_client.translate(text,source_language=self.source,target_language=self.target)
+  
+        return result["translatedText"]
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+     
+
+
+def listen_print_loop(responses,token,stream):
+    
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        transcript = result.alternatives[0].transcript
+
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write("Speech:"+transcript + overwrite_chars + "\r")
+            sys.stdout.flush()
+    
+            num_chars_printed = len(transcript)
+
+        else:
+            print("Speech:",transcript + overwrite_chars)
+            sentence=transcript + overwrite_chars
+            
+            print("Translate:",stream.zoomtranslate(sentence),"\n")
+
+            sentence=sentence+"\n"+stream.zoomtranslate(sentence)
+            post_params={
+                'seq' : stream.seq_count,
+                'lang':"en-US"}
+            headers={'Content-type': 'text/plain; charset=utf-8'}
+
+            result=requests.post(token,params=post_params, data=sentence.encode('utf-8'),headers=headers)
+            if result.status_code!=200:
+                print(">>錯誤！訊息為傳送出去！")
+            stream.seq_count=stream.seq_count+1
+            num_chars_printed = 0
+
+
+def main():
+    token=input("請輸入zoom API憑證：")
+    print()
+    with open('config.json') as config:
+        _configs = json.load(config)
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+
+    # 請在這裡放入常用語詞進行判斷
+    phrases = ["中央長老團","會眾","分區監督","分部委員會"] 
+    boost = 20.0
+    speech_contexts_element = {"phrases": phrases, "boost": boost}
+    speech_contexts = [speech_contexts_element]
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        speech_contexts=speech_contexts,
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=_configs["source_lang"],
+        enable_automatic_punctuation=True
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+    print("請開始說話.............. (結束請按下ctrl+c)：")
+    print()
+    with MicrophoneStream(RATE, CHUNK,_configs) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        
+        listen_print_loop(responses,token,stream)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('---------------------結束程式---------------------')
+        sys.exit(0)
+# [END speech_transcribe_streaming_mic]
