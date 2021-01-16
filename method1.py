0e7367fd7b3c2750bf2efa482c028a2f771440ec
xellosiris <xellosiris@gmail.com>
2021-01-16 19:13:03 +0800

    update requests session

--- method1.py
+++ method1.py
@@ -101,28 +101,13 @@ class MicrophoneStream(object):
 
 def listen_print_loop(responses,token,stream):
     
-    """Iterates through server responses and prints them.
 
-    The responses passed is a generator that will block until a response
-    is provided by the server.
-
-    Each response may contain multiple results, and each result may contain
-    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
-    print only the transcription for the top alternative of the top result.
-
-    In this case, responses are provided for interim results as well. If the
-    response is an interim one, print a line feed at the end of it, to allow
-    the next result to overwrite it, until the response is a final one. For the
-    final one, print a newline to preserve the finalized transcription.
-    """
     num_chars_printed = 0
     for response in responses:
         if not response.results:
             continue
 
-        # The `results` list is consecutive. For streaming, we only care about
-        # the first result being considered, since once it's `is_final`, it
-        # moves on to considering the next utterance.
+        
         result = response.results[0]
         if not result.alternatives:
             continue
@@ -130,11 +115,6 @@ def listen_print_loop(responses,token,stream):
         # Display the transcription of the top alternative.
         transcript = result.alternatives[0].transcript
 
-        # Display interim results, but with a carriage return at the end of the
-        # line, so subsequent lines will overwrite them.
-        #
-        # If the previous result was longer than this one, we need to print
-        # some extra spaces to overwrite the previous result
         overwrite_chars = " " * (num_chars_printed - len(transcript))
 
         if not result.is_final:
@@ -154,8 +134,16 @@ def listen_print_loop(responses,token,stream):
                 'seq' : stream.seq_count,
                 'lang':"en-US"}
             headers={'Content-type': 'text/plain; charset=utf-8'}
-
-            result=requests.post(token,params=post_params, data=sentence.encode('utf-8'),headers=headers)
+            if stream.seq_count == 0:
+                print("初次建立連線！")
+                session = requests.Session()
+                s=time.time()
+                result=session.post(token,params=post_params, data=sentence.encode('utf-8'),headers=headers)
+                print(f"第{stream.seq_count}次傳送花費：{time.time()-s:.2f}")
+            else:
+                s=time.time()
+                result=session.post(token,params=post_params, data=sentence.encode('utf-8'),headers=headers)
+                print(f"第{stream.seq_count}次傳送花費：{time.time()-s:.2f}")
             if result.status_code!=200:
                 print(">>錯誤！訊息為傳送出去！")
             stream.seq_count=stream.seq_count+1
